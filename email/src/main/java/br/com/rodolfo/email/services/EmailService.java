8df148426b7845151ace5da1d5f7ec9141860e68
Rodolfo Bortolozo <rodolfobortolozo@gmail.com>
2023-10-23 21:00:12 -0300

    Log4j2

--- email/src/main/java/br/com/rodolfo/email/services/EmailService.java
+++ email/src/main/java/br/com/rodolfo/email/services/EmailService.java
@@ -1,10 +1,11 @@
 package br.com.rodolfo.email.services;
 
-import br.com.rodolfo.email.dto.EmailDto;
 import br.com.rodolfo.email.enums.StatusEmail;
 import br.com.rodolfo.email.models.Email;
 import br.com.rodolfo.email.repositories.EmailRepository;
 import jakarta.transaction.Transactional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -19,6 +20,8 @@ public class EmailService {
     private final EmailRepository emailRepository;
     private final JavaMailSender emailSender;
 
+    Logger logger = LogManager.getLogger(EmailService.class);
+
     public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
         this.emailRepository = emailRepository;
         this.emailSender = emailSender;
@@ -30,6 +33,12 @@ public class EmailService {
     @Transactional
     public Email sendEmail(Email email) {
         try{
+            logger.trace("TRACE");
+            logger.debug("DEBUG");
+            logger.info("INFO");
+            logger.warn("WARN");
+            logger.error("ERROR");
+            logger.fatal("FATAL");
             email.setSendDateEmail(LocalDateTime.now());
             email.setEmailFrom(emailFrom);
 
